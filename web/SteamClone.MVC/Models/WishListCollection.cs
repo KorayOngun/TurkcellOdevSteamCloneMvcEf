78cf78e237839d23587c377be5ea42835459000a
Koray öngün <90770831+KorayOngun@users.noreply.github.com>
2023-06-10 17:36:38 +0300

    Update WishListCollection.cs

--- web/SteamClone.MVC/Models/WishListCollection.cs
+++ web/SteamClone.MVC/Models/WishListCollection.cs
@@ -26,7 +26,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public string ImageUrl { get; set; } = "https://loremflickr.com/320/240";
+        public string ImageUrl { get; set; } 
+        
         public int Quantity { get; set; } = 1;
     }
 }
